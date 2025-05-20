a5ce629e44b9f7879d73784e23df9f511fa28b6a
Bang <thbang727@gmail.com>
2025-05-20 20:45:20 +0700

    search in artist song and album

--- lib/pages/artist/artist_page.dart
+++ lib/pages/artist/artist_page.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 
 import 'package:memecloud/apis/supabase/main.dart';
 import 'package:memecloud/blocs/song_player/song_player_cubit.dart';
+import 'package:memecloud/components/artist/album_list_tile.dart';
 import 'package:memecloud/components/artist/song_list_tile.dart';
 import 'package:memecloud/components/miscs/default_future_builder.dart';
 import 'package:memecloud/components/song/mini_player.dart';
@@ -17,6 +18,7 @@ import 'package:memecloud/models/artist_model.dart';
 
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:memecloud/components/miscs/expandable_html.dart';
+import 'package:memecloud/pages/artist/album_artist_page.dart';
 
 import 'package:memecloud/pages/artist/song_artist_page.dart';
 
@@ -471,7 +473,7 @@ class _AlbumsOfArtist extends StatelessWidget {
               ),
               itemBuilder: (context, index) {
                 final album = albums[index];
-                return _AlbumCard(album: album);
+                return AlbumListTile(album: album);
               },
               itemCount: albums.length > 4 ? 4 : albums.length,
             ),
@@ -481,124 +483,18 @@ class _AlbumsOfArtist extends StatelessWidget {
           TextButton.icon(
             onPressed:
                 () => {
-                  // Navigator.push(
-                  //   context,
-                  //   MaterialPageRoute(
-                  //     builder: (context) => SongArtistPage(songs: albums),
-                  //   ),
-                  // ),
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => AlbumArtistPage(albums: albums),
+                    ),
+                  ),
                 },
             label: Text("Xem thêm"),
             icon: Icon(Icons.arrow_right),
           ),
+        const SizedBox(height: 80),
       ],
-    );
-  }
-}
-
-class _AlbumCard extends StatelessWidget {
-  final PlaylistModel album;
-  const _AlbumCard({required this.album});
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      elevation: 4,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-      clipBehavior: Clip.antiAlias,
-      color: Theme.of(context).colorScheme.onSecondary,
-      child: InkWell(
-        onTap: () {
-          //TO DO: play album
-        },
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Expanded(
-              child: Stack(
-                children: [
-                  Hero(
-                    tag: 'album_${album.id}',
-                    child: CachedNetworkImage(
-                      imageUrl: album.thumbnailUrl,
-                      fit: BoxFit.cover,
-                      width: double.infinity,
-                      placeholder:
-                          (context, url) => Container(
-                            color: Colors.grey[300],
-                            child: const Center(
-                              child: CircularProgressIndicator(),
-                            ),
-                          ),
-                      errorWidget:
-                          (context, url, error) => Container(
-                            color: Colors.grey[300],
-                            child: const Icon(
-                              Icons.album,
-                              size: 48,
-                              color: Colors.grey,
-                            ),
-                          ),
-                    ),
-                  ),
-
-                  Positioned.fill(
-                    child: Material(
-                      color: Colors.transparent,
-                      child: InkWell(
-                        onTap: () {
-                          // TO DO: play album
-                        },
-                        child: Center(
-                          child: Container(
-                            padding: const EdgeInsets.all(12),
-                            decoration: BoxDecoration(
-                              color: Colors.white.withOpacity(0.2),
-                              borderRadius: BorderRadius.circular(30),
-                            ),
-                            child: const Icon(
-                              Icons.play_arrow,
-                              size: 48,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(
-                    album.title,
-                    style: const TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  if (album.description != null) ...[
-                    const SizedBox(height: 4),
-                    Text(
-                      album.description!,
-                      style: TextStyle(fontSize: 12, color: Colors.grey[600]),
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ],
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
