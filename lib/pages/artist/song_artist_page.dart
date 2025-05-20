821bd847e79a7b29433d5b326e2e82094c3f4fba
Bang <thbang727@gmail.com>
2025-05-20 10:24:11 +0700

    back to top

--- lib/pages/artist/song_artist_page.dart
+++ lib/pages/artist/song_artist_page.dart
@@ -1,18 +1,60 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:memecloud/blocs/song_player/song_player_cubit.dart';
-import 'package:memecloud/blocs/song_player/song_player_state.dart';
+
 import 'package:memecloud/components/artist/song_list_tile.dart';
-import 'package:memecloud/components/song/play_or_pause_button.dart';
-import 'package:memecloud/core/getit.dart';
 
 import 'package:memecloud/models/song_model.dart';
 
-class SongArtistPage extends StatelessWidget {
+class SongArtistPage extends StatefulWidget {
   final List<SongModel> songs;
 
   const SongArtistPage({super.key, required this.songs});
+
+  @override
+  State<SongArtistPage> createState() => _SongArtistPageState();
+}
+
+class _SongArtistPageState extends State<SongArtistPage> {
+  final ScrollController _scrollController = ScrollController();
+  bool _showBackToTopButton = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_listenToScroll);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_listenToScroll);
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _scrollToTop() {
+    _scrollController.animateTo(
+      0,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeInOut,
+    );
+  }
+
+  void _listenToScroll() {
+    _scrollController.addListener(() {
+      if (_scrollController.offset >= 300) {
+        if (!_showBackToTopButton) {
+          setState(() {
+            _showBackToTopButton = true;
+          });
+        }
+      } else {
+        if (_showBackToTopButton) {
+          setState(() {
+            _showBackToTopButton = false;
+          });
+        }
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,20 +62,33 @@ class SongArtistPage extends StatelessWidget {
     final colorScheme = theme.colorScheme;
 
     return Scaffold(
+      floatingActionButton:
+          _showBackToTopButton
+              ? FloatingActionButton(
+                onPressed: () {
+                  _scrollController.animateTo(
+                    0,
+                    duration: Duration(milliseconds: 500),
+                    curve: Curves.easeInOut,
+                  );
+                },
+                tooltip: "Về đầu trang",
+                child: Icon(Icons.arrow_upward),
+              )
+              : null,
       body: CustomScrollView(
         slivers: [
           const SliverAppBar(title: Text("Các bài hát")),
-          // Songs list
           SliverPadding(
             padding: const EdgeInsets.all(16),
             sliver: SliverList(
               delegate: SliverChildBuilderDelegate((context, index) {
-                final song = songs[index];
+                final song = widget.songs[index];
                 return Padding(
                   padding: const EdgeInsets.only(bottom: 8),
                   child: SongListTile(song: song),
                 );
-              }, childCount: songs.length),
+              }, childCount: widget.songs.length),
             ),
           ),
         ],
